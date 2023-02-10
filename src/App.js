3437744462ba8b460628dfbfb439bd02a95cb56d
Thameem Ansari <loyalty.thameem@gmail.com>
2023-02-10 05:43:57 +0000

    Service worker App.js

--- src/App.js
+++ src/App.js
@@ -35,81 +35,123 @@
 //   );
 // }
 
+// import React, { useState } from "react";
+
+// const App = () => {
+//   const [showNotification, setShowNotification] = useState(false);
+//   const [notification, setNotification] = useState(null);
+//   console.log("showNotification", showNotification);
+//   console.log("notification", notification);
+//   const handleShowNotification = () => {
+//     if (window.Notification && Notification.permission === "granted") {
+//       var options = {
+//         body: "The leader",
+//         icon: "https://avatars.githubusercontent.com/u/97792702?v=4    auto=compress&cs=tinysrgb&dpr=1&w=500",
+//         dir: "ltr",
+//       };
+//       const newNotification = new Notification("Thameem ansari!", options);
+//       setNotification(newNotification);
+//       setShowNotification(true);
+//     } else if (window.Notification && Notification.permission !== "denied") {
+//       Notification.requestPermission().then((permission) => {
+//         var options = {
+//           body: "The leader",
+//           icon: "https://avatars.githubusercontent.com/u/97792702?v=4    auto=compress&cs=tinysrgb&dpr=1&w=500",
+//           dir: "rtl",
+//         };
+//         if (permission === "granted") {
+//           const newNotification = new Notification("Thameem ansari!", options);
+//           setNotification(newNotification);
+//           setShowNotification(true);
+//         }
+//       });
+//     } else {
+//       alert("Please give notification permission");
+//     }
+//   };
+
+//   const handleHideNotification = () => {
+//     notification.close();
+//     setShowNotification(false);
+//   };
+
+//   const handleNewNotification = () => {
+//     if (window.Notification && Notification.permission === "granted") {
+//       var options = {
+//         body: "The leader",
+//         icon: "https://avatars.githubusercontent.com/u/97792702?v=4    auto=compress&cs=tinysrgb&dpr=1&w=500",
+//         dir: "ltr",
+//         image: "https://wallpaperaccess.com/full/3225964.jpg",
+//         data: { id: 1, name: "Thameem", lastName: "ansari" },
+//       };
+//       const newNotification = new Notification("Thameem ansari!", options);
+//       setNotification(newNotification);
+//       setShowNotification(true);
+//       //Navigation
+//       newNotification.onclick = (e) => {
+//         // Navigate by id
+//         window.location.href =
+//           "https://0jhk0v-3000.preview.csb.app/" + e.target.data.id;
+//         alert(e.target.data.name + " " + e.target.data.lastName);
+//       };
+//     } else {
+//       alert("Please give notification permission");
+//     }
+//   };
+
+//   return (
+//     <div>
+//       {!showNotification && (
+//         <button onClick={handleShowNotification}>Show Notification </button>
+//       )}
+//       {showNotification && (
+//         <button onClick={handleHideNotification}> Hide Notification </button>
+//       )}
+//       <button onClick={handleNewNotification}> New Notification </button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import React, { useState } from "react";
 
-const App = () => {
+const DesktopNotification = () => {
   const [showNotification, setShowNotification] = useState(false);
-  const [notification, setNotification] = useState(null);
-  console.log("showNotification", showNotification);
-  console.log("notification", notification);
+
   const handleShowNotification = () => {
-    if (window.Notification && Notification.permission === "granted") {
-      var options = {
-        body: "The leader",
-        icon: "https://avatars.githubusercontent.com/u/97792702?v=4    auto=compress&cs=tinysrgb&dpr=1&w=500",
-        dir: "ltr",
-      };
-      const newNotification = new Notification("Thameem ansari!", options);
-      setNotification(newNotification);
-      setShowNotification(true);
-    } else if (window.Notification && Notification.permission !== "denied") {
-      Notification.requestPermission().then((permission) => {
-        var options = {
-          body: "The leader",
-          icon: "https://avatars.githubusercontent.com/u/97792702?v=4    auto=compress&cs=tinysrgb&dpr=1&w=500",
-          dir: "rtl",
-        };
-        if (permission === "granted") {
-          const newNotification = new Notification("Thameem ansari!", options);
-          setNotification(newNotification);
-          setShowNotification(true);
-        }
-      });
-    } else {
-      alert("Please give notification permission");
+    if (!("serviceWorker" in navigator)) {
+      console.log("serviceWorker", navigator);
+      return;
     }
-  };
 
-  const handleHideNotification = () => {
-    notification.close();
-    setShowNotification(false);
-  };
-
-  const handleNewNotification = () => {
-    if (window.Notification && Notification.permission === "granted") {
-      var options = {
-        body: "The leader",
+    navigator.serviceWorker.ready.then((registration) => {
+      console.log("serviceWorker", registration);
+      registration.showNotification("Thameem ansari!", {
+        body: "I'm Leader",
         icon: "https://avatars.githubusercontent.com/u/97792702?v=4    auto=compress&cs=tinysrgb&dpr=1&w=500",
         dir: "ltr",
         image: "https://wallpaperaccess.com/full/3225964.jpg",
         data: { id: 1, name: "Thameem", lastName: "ansari" },
-      };
-      const newNotification = new Notification("Thameem ansari!", options);
-      setNotification(newNotification);
+      });
       setShowNotification(true);
-      //Navigation
-      newNotification.onclick = (e) => {
-        // Navigate by id
-        window.location.href =
-          "https://0jhk0v-3000.preview.csb.app/" + e.target.data.id;
-        alert(e.target.data.name + " " + e.target.data.lastName);
-      };
-    } else {
-      alert("Please give notification permission");
-    }
+    });
+  };
+
+  const handleHideNotification = () => {
+    setShowNotification(false);
   };
 
   return (
     <div>
       {!showNotification && (
-        <button onClick={handleShowNotification}>Show Notification </button>
+        <button onClick={handleShowNotification}> Show Notification </button>
       )}
       {showNotification && (
         <button onClick={handleHideNotification}> Hide Notification </button>
       )}
-      <button onClick={handleNewNotification}> New Notification </button>
     </div>
   );
 };
 
-export default App;
+export default DesktopNotification;
